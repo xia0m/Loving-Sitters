2d7087c11f0770ca0d11950cc75f9f430a8b92a1
xia0m <alexma325@gmail.com>
2021-07-08 23:19:30 -0700

    fix app.js bug

--- server/app.js
+++ server/app.js
@@ -44,11 +44,11 @@ app.use("/conversations", conversationRouter);
 app.use("/messages", messageRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
-  );
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running");
